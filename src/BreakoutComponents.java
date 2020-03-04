8b8dce8599dc7f9cc9f2f29800e9cb7ca2083fc7
lu <fooooxxxxlu@gmail.com>
2020-03-04 09:03:24 +0800

    添加了Brick类

--- src/BreakoutComponents.java
+++ src/BreakoutComponents.java
@@ -2,10 +2,12 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
 public class BreakoutComponents extends JComponent {
     Paddle paddle;
     Ball ball;
+    ArrayList<Brick> bricks;
 
     BreakoutComponents(Paddle paddle, Ball ball) {
         this.paddle = paddle;
--- src/Brick.java
+++ src/Brick.java
@@ -1,7 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+
 /** 砖块类
  *
  */
-public class Brick {
+public class Brick extends JComponent {
 
     /** Width of a brick */
     private int BRICK_WIDTH;
@@ -10,4 +13,61 @@ public class Brick {
     /** 是否存在 */
     private boolean isAlive = true;
 
+    private int x,y;
+    private Color color;
+
+    public void draw(Graphics g){
+        Graphics2D g2 = (Graphics2D)g;
+        if(isAlive){
+            g2.setColor(color);
+            g2.fillRect(x,y,BRICK_WIDTH,BRICK_HEIGHT);
+        }
+    }
+
+    public Brick() {
+    }
+
+    public int getBRICK_WIDTH() {
+        return BRICK_WIDTH;
+    }
+
+    public void setBRICK_WIDTH(int BRICK_WIDTH) {
+        this.BRICK_WIDTH = BRICK_WIDTH;
+    }
+
+    public static int getBrickHeight() {
+        return BRICK_HEIGHT;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
 }
