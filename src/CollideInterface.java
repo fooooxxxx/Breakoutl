b9aea667604f41555bca30f515ca3217b4bc4e06
lu <fooooxxxxlu@gmail.com>
2020-03-21 19:20:26 +0800

    新建碰撞接口类

---
+++ src/CollideInterface.java
@@ -0,0 +1,13 @@
+/**
+ *
+ */
+public interface CollideInterface {
+    /** 判断小球是否碰撞函数
+     * @param object_x 被判断物体的x轴起始坐标
+     * @param object_y 被判断物体的y轴起始坐标
+     * @param object_height 被判断物体的高度
+     * @param object_width 被判断物体的宽度
+     * @return 返回true,则表示发生了碰撞,否则无碰撞*/
+    public boolean collide(int object_x,int object_y,int object_width,int object_height);
+
+}
