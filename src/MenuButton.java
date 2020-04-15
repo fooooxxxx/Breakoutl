5a54ec5b2f82bfc4a2bfa5d8234a910363f4bd80
lu <fooooxxxxlu@gmail.com>
2020-04-15 10:08:14 +0800

    美化失败

--- src/MenuButton.java
+++ src/MenuButton.java
@@ -11,23 +11,23 @@ import java.awt.event.MouseEvent;
 public class MenuButton extends JButton {
     Color defaultColor = new Color(255,111,0);//默认字体颜色,以及鼠标移到按钮上后背景颜色
     Color enterFontColor = Color.BLACK;//鼠标移动到按钮上后字体颜色
+    Color pressBackgroundColor = new Color(252,158,86);//按下后背景颜色
+    ImageIcon pressIcon = new ImageIcon("src/image/press_button_background.png");
 
     MenuButton(){
-        this.setOpaque(false);//内容透明
-        this.setContentAreaFilled(false);
-        setBorderPainted(false);//去边框
         setMoveEffect();
     }
     MenuButton(String text){
         super(text);
-        this.setOpaque(false);
-        this.setContentAreaFilled(false);
-        setBorderPainted(false);
         setMoveEffect();
     }
 
     void setMoveEffect(){
+        this.setOpaque(false);//内容透明
+        this.setContentAreaFilled(false);
+        setBorderPainted(false);//去边框
         setForeground(defaultColor);
+        setPressedIcon(pressIcon);//无效
         addMouseListener(new MouseAdapter() {
             @Override
             public void mouseEntered(MouseEvent e) {//鼠标进入后
@@ -44,6 +44,7 @@ public class MenuButton extends JButton {
                 setBackground(enterFontColor);
                 setForeground(defaultColor);
             }
+
         });
     }
 
